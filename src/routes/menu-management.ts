79f4eeff9c033e9b6776649831be9adce9a4d264
achi1302 <alexanderson.chacon@gmail.com>
2024-03-27 20:29:48 -0700

    +Two methods in the back-end created, one handles the recipes and the other one the ingredients. The recipes sends the newly created id so it can be grabbed by ingredients. In the Front-end there is a more complete Box that lets the user write a complete recipe with how many ingredients the user wants by creating a for loop manipulating the 'selectedMaterials' and using the recipeId. Form Control to be fixed next, it does not display the other options.

--- src/routes/menu-management.ts
+++ src/routes/menu-management.ts
@@ -11,4 +11,52 @@ router.get('/testing', async (req: Request, res: Response) => {
 
 
 
+// router.post('/add_recipe_and_ingredients', async (req: Request, res: Response) => {
+//     try {
+//         // Extract data from the request body
+//         const { option, name, description, price, categories, productId, quantity } = req.body;
+//
+//         // Call the stored procedure
+//         const query = 'CALL insert_recipe_and_ingredients($1, $2, $3, $4, $5, $6, $7)';
+//         const values = [option, name, description, price, categories, productId, quantity];
+//         await pool.query(query, values);
+//
+//         res.status(200).send({msg: "Operation successful"});
+//     } catch (error: any) {
+//         console.error('Error:', error.message);
+//         res.status(500).json({ error: 'An error occurred. Please try again.' });
+//     }
+// });
+
+router.post('/add_recipe', async (req, res) => {
+    try {
+        const {name, description, price, categories} = req.body;
+        const query = 'SELECT add_recipe($1, $2, $3, $4)';
+        const values = [name, description, price, categories];
+
+        const result = await pool.query(query, values);
+        const recipeId = result.rows[0].add_recipe;
+
+        res.status(200).send({ msg: 'Recipe added successfully', recipeId: recipeId });
+    } catch (error:any) {
+        console.error('Error:', error.message);
+        res.status(500).send({ error: 'An error occurred. Please try again.' });
+    }
+});
+
+
+router.post('/add_ingredient', async (req, res) => {
+    try {
+        const {recipeId, productId, quantity} = req.body;
+        const query = 'SELECT add_ingredient($1, $2, $3)';
+        const values = [recipeId, productId, quantity];
+
+        await pool.query(query, values);
+        res.status(200).send({ msg: 'Ingredient added successfully.' });
+    } catch (error:any) {
+        console.error('Error:', error.message);
+        res.status(500).send({ error: 'An error occurred. Please try again.' });
+    }
+});
+
 export default router;
