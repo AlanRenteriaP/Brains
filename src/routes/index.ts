f1113b754976cbeb3df7e3a7f480e479e9cc194a
beatrenger <arenteriaperulles@outlook.com>
2024-04-02 16:59:24 -0700

    working on menuData

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,12 @@
 import express from 'express';
 import authRoutes from './auth';
 import invManagementRoutes from './inv-management';
-
 import menuManagementRoutes from './menu-management';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/invmanagement', invManagementRoutes);
-
 router.use('/menumanagement', menuManagementRoutes);
 
 export default router;
--- src/routes/menu-management.ts
+++ src/routes/menu-management.ts
@@ -9,6 +9,45 @@ router.get('/testing', async (req: Request, res: Response) => {
     res.json({ 'itsworking':'yes' });
 });
 
+function isErrorWithMessage(error: unknown): error is { message: string } {
+    return typeof error === 'object' && error !== null && 'message' in error;
+}
 
+
+router.get('/menuData', async (req, res) => {
+    try {
+        const recipeQuery = `
+            SELECT
+                r.id,
+                'https://fakeimg.pl/300x300/?text=Proyecto_Xocolate' AS image,
+                r.name AS title,
+                r.description,
+                r.price AS sellingPrice,
+                r.categories
+            FROM
+                recipes r;
+        `;
+        const recipesResult = await pool.query(recipeQuery);
+        const recipesWithCategories = recipesResult.rows.map(recipe => {
+            // Adjust based on the actual data type of `categories`
+            const categories = Array.isArray(recipe.categories) ? recipe.categories.join(', ') : recipe.categories;
+            return {
+                ...recipe,
+                category: categories // Directly use or adjust based on your needs
+            };
+        });
+
+        res.status(200).json(recipesWithCategories);
+    }  catch (error: unknown) {
+        if (isErrorWithMessage(error)) {
+            console.error('Error getting menu data:', error.message);
+            res.status(500).json({ error: 'An error occurred while fetching the menu data. Please try again.', details: error.message });
+        } else {
+            // Handle the case where the error does not have a message
+            console.error('An unexpected error occurred:', error);
+            res.status(500).json({ error: 'An unexpected error occurred. Please try again.' });
+        }
+    }
+});
 
 export default router;
